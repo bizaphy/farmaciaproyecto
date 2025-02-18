1b0acbe843dc0f77d342fb331a3527a731ae0141
Bizaphy <110700808+bizaphy@users.noreply.github.com>
2025-02-18 09:31:14 -0300

    v2.11

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 // 📌 URL BASE DEL BACKEND --> RENDER
-const API_URL = "https://farmaciaproyecto.onrender.com/api/";
+const API_URL = "https://farmaciaproyecto.onrender.com/api/auth";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
